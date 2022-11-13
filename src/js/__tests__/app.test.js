f46ed4747f678992577308094adf633c1f602c6a
Your Name <you@example.com>
2022-11-13 19:12:38 +0300

    add test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,29 @@
-import sum from '../basic';
+import orderByProps from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+const obj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
 
-  expect(result).toBe(6);
+const arrOrder = ['name', 'level'];
+
+const printres = [
+  { key: 'name', value: 'мечник' },
+  { key: 'level', value: 2 },
+  { key: 'attack', value: 80 },
+  { key: 'defence', value: 40 },
+  { key: 'health', value: 10 },
+];
+
+describe('testing fuction orderByProps', () => {
+  const result = orderByProps(obj, arrOrder);
+  test('arrSort with toEqual', () => {
+    expect(result).toEqual(printres);
+  });
+  test('arrSort with toBe', () => {
+    expect(result).not.toBe(printres);
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,31 @@
-const obj = {
-    name: 'мечник',
-    health: 10, 
-    level: 2, 
-    attack: 80, 
-    defence: 40
-};
-
-const arrOrder = ["name", "level"];
-
-export default function orderByProps (object, arr ) {
-    const character = [];
-    const data = {...object};
-    for (const item of arr) {
-        if (item in data) {
-            character.push(quality(item, data[item]));
-            delete data[item];
-        }
-    }
-    const dataSort = Object.keys(data).sort().reduce(
-        (obj, key) => { 
-          obj[key] = data[key]; 
-          return obj;
-        }, 
-        {}
-      );
-    for (const item in dataSort) {
-        character.push(quality(item, dataSort[item]));
-    }
-    return character;
+function quality(key, value) {
+  return {
+    key,
+    value,
+  };
 }
 
-function quality(key, value) {
-    return {
-        key: key,
-        value: value
-    };
+export default function orderByProps(object, arr) {
+  const character = [];
+  const data = { ...object };
+  for (const item of arr) {
+    if (item in data) {
+      character.push(quality(item, data[item]));
+      delete data[item];
+    }
+  }
+  const dataSort = Object.keys(data).sort().reduce(
+    (localobj, key) => {
+      const newobj = localobj;
+      newobj[key] = data[key];
+      return newobj;
+    },
+    {},
+  );
+  for (const item in dataSort) {
+    if (item) {
+      character.push(quality(item, dataSort[item]));
+    }
+  }
+  return character;
 }
